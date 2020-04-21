01cb282376a24f514aec950c697178ca78283763
madoci <36835658+madoci@users.noreply.github.com>
2020-04-21 14:08:48 +0200

    Remote is now origin-travis to avoid conflict

--- scripts/push_pom.sh
+++ scripts/push_pom.sh
@@ -12,5 +12,5 @@ git add pom.xml
 git commit -m "Prepare next release"
 
 # Push pom.xml
-git remote set-url origin https://${GH_TOKEN}@github.com/madoci/toast.git
-git push --quiet --set-upstream origin master
+git remote add origin-travis https://${GH_TOKEN}@github.com/madoci/toast.git
+git push --quiet --set-upstream origin-travis master
